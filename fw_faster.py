42581e941430abbf709b9ae27e0c3e4a67fb8f50
Simanis Roberts <roberts.simanis@swisscom.com>
2025-02-20 09:20:56 +0200

    added hotkey support and adjusted timings

--- fw_faster.py
+++ fw_faster.py
@@ -1,8 +1,9 @@
 import pyautogui
 import time
+from pynput import keyboard
 
 # Set end date for FW tasks
-END_DATE = "18.02.2025 18:00:00"
+END_DATE = "27.02.2025 18:00:00"
 
 def pointAt(img):
     while True:
@@ -12,7 +13,7 @@ def pointAt(img):
                 return pyautogui.center(button_location)
         except pyautogui.ImageNotFoundException:
             pass
-        time.sleep(0.5)
+        time.sleep(0.5) 
 
 # Function to click a button when it appears
 def safe_click(img):
@@ -35,31 +36,59 @@ def safe_doubleClick(img):
             print(f"Failed to click {img}: {e}")
             time.sleep(0.5)
 
-time.sleep(2)
+# Global variable to signal when the key is pressed
+key_pressed = False
 
+def on_press(key):
+    global key_pressed, ctrl_held
+    try:
+        # Check if Ctrl is pressed
+        if key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
+            ctrl_held = True
+        # Check if F10 is pressed while Ctrl is held down
+        elif key == keyboard.Key.f10 and ctrl_held:
+            print("Ctrl + F10 pressed, continuing...")
+            key_pressed = True
+            return False  # Stop listener after the key is detected
+    except AttributeError:
+        pass
+
+def on_release(key):
+    global ctrl_held
+    try:
+        # Check if Ctrl is released
+        if key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
+            ctrl_held = False
+    except AttributeError:
+        pass
+
+# Function to wait for the key press
+def wait_for_key_press():
+    global key_pressed
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+    while not key_pressed:
+        time.sleep(0.1)
+
+time.sleep(2)
 # Collect all instances of the button
 buttons = list(pyautogui.locateAllOnScreen('dropdown.png'))
-
 # assign change coordinator
 pyautogui.click(pyautogui.center(buttons[1]))
 time.sleep(1)
 pyautogui.click(pointAt('name.png'))
-
 # assign change manager
 pyautogui.click(pyautogui.center(buttons[2]))
 time.sleep(1)
 pyautogui.click(pointAt('name.png'))
-
 # Open task
 safe_click('tasks.png')
 safe_doubleClick('pmatrix_task.png')
 safe_click('save_con.png')
-time.sleep(2)  # Might still need a small delay here if the next element appears immediately after saving
-
+time.sleep(2)
 ## Process task
 # Set dates
 safe_click('dates.png')
-
 # Clear and set start date
 pyautogui.moveTo(pointAt('start_date.png'))
 pyautogui.moveRel(277, 0)
@@ -67,25 +96,22 @@ pyautogui.click()
 pyautogui.hotkey('ctrl', 'a')
 pyautogui.press('delete')
 pyautogui.press('enter')
-
 # Set end date
 pyautogui.moveRel(500, 0)
 pyautogui.click()
 pyautogui.hotkey('ctrl', 'a')
 pyautogui.press('delete')
 pyautogui.write(END_DATE)
-
 # Assign to person
 safe_click('assignment.png')
-input("Assign the engineer, press enter to continue")
+
+print("Assign the engineer, press Ctrl + F10 to continue")
+wait_for_key_press()
+
 safe_click('save_task.png')
-
 safe_click('ok.png')
-
-#time.sleep(1)
 safe_click('back.png')
-#time.sleep(1)
-
+time.sleep(1.5)
 # Set CRQ dates
 pyautogui.moveTo(pointAt('start_date_crq.png'))
 pyautogui.moveRel(277, 0)
@@ -93,18 +119,13 @@ pyautogui.click()
 pyautogui.hotkey('ctrl', 'a')
 pyautogui.press('delete')
 pyautogui.press('enter')
-
-time.sleep(1)
 pyautogui.moveRel(0, 30)
 pyautogui.click()
 safe_click('close.png')
-
 pyautogui.moveTo(pointAt('start_date_crq.png'))
 pyautogui.moveRel(277, 0)
 pyautogui.moveRel(0, 30)
 pyautogui.click()
-
-#time.sleep(1)
 pyautogui.hotkey('ctrl', 'a')
 pyautogui.press('delete')
 pyautogui.write(END_DATE)
@@ -112,24 +133,18 @@ pyautogui.write(END_DATE)
 # Move to scheduled and save
 pyautogui.moveRel(-300, 0)
 pyautogui.scroll(-200)
-time.sleep(1)  # Small delay might be needed for the page to update
+time.sleep(1)  # Scroll delay
 pyautogui.scroll(-200)
 safe_click('next_stage.png')
 safe_click('yes.png')
 safe_click('ok.png')
-#time.sleep(1)
 safe_click('next_stage.png')
 safe_click('ok.png')
-
-#time.sleep(1)
-#pyautogui.scroll(-200)  # Small delay might be needed for the page to update
-#pyautogui.scroll(-200)
-
 safe_click('save_crq.png')
 safe_click('yes_2.png')
 
 pyautogui.moveRel(-300, 0)
-time.sleep(0.6)
+time.sleep(1)
 pyautogui.scroll(200)
 pyautogui.scroll(200)
 
